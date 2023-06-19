b11447ac0ddeaa6ca41e8ecd69d85a98307721b4
saleh <animatorsaleh@gmail.com>
2023-06-20 03:04:05 +0330

    Add handle and value changer

--- asset_tools/sinrange_base.js
+++ asset_tools/sinrange_base.js
@@ -9,7 +9,7 @@ class SinRangeInput{
 	 * @param {Number} sinSVGHeight Height of the svg element
 	 */
 
-	constructor({holderID, sinRepeatsCount=1} = {}){
+	constructor({holderID, sinRepeatsCount=1, percentageValue=0} = {}){
 		/**
 		 * constructor of sinRangeInput
 		 * @argument {String} holderID the ID of HTML element holder of range selector
@@ -19,13 +19,17 @@ class SinRangeInput{
 		this.segments = 1000;
 		this.sinRepeatsCount = sinRepeatsCount
 		this.holderElement = document.getElementById(holderID);
+		this.svgHolder = null
+		this.sinGroup = null;
 
-		this.strokeWidth = 5
+		this.strokeWidth = 15
 		this.sinSVGWidth = this.holderElement.clientWidth;
 		this.sinSVGHeight = this.holderElement.clientHeight;
 
-
+		this.percentageValue = percentageValue
 		this.amplitudeMultiplier = this.getAmplitudeMultiplier()
+
+		this.clickHold = false;
 	}
 
 	getSinYAxis(offsetX){
@@ -47,43 +51,124 @@ class SinRangeInput{
 		return this.sinSVGWidth / ( 2 * Math.PI * this.sinRepeatsCount)
 	}
 
+	PercentageOffsetX(percentage){
+		return this.svgHolder.clientWidth * percentage / 100
+	}
+
+	PercentageSinX(percentage){
+		let ratio = percentage/100;
+		let step = Math.floor(this.segments * ratio);
+		return this.getRelativeSinOffset(step);
+	}
+
+	PercentageSinY(percentage){
+		return this.getSinYAxis(this.PercentageSinX(percentage));
+	}
+
 	describeSinPath({percentage=100}={}){
 		let path = []
-		let currentOffsetX = 0
-		let NextOffsetX = 0
+		let currentOffsetX = 0;
+		let NextOffsetX = 0;
 		let xAxisRatio = this.getScreenToCartesianRatio();
+
 		for (let i=0; i<this.segments; i++){
 			currentOffsetX = this.getRelativeSinOffset(i);
 			NextOffsetX = this.getRelativeSinOffset(i+1);
 			path.push(
-				'M', xAxisRatio * currentOffsetX, this.getSinYAxis(currentOffsetX),
-				'L', xAxisRatio * NextOffsetX, this.getSinYAxis(NextOffsetX)
+				'M', xAxisRatio * currentOffsetX + this.strokeWidth/2, this.getSinYAxis(currentOffsetX),
+				'L', xAxisRatio * NextOffsetX + this.strokeWidth/2, this.getSinYAxis(NextOffsetX)
 			)
 		}
 
 		return path.join(" ")
 	}
 
-	generateSVG(){
-		let svgHolder = this.holderElement
+	draw(){
+		this.svgHolder = this.holderElement
 		const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 		const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-		const sinGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+		this.sinGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
 
 		svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-        svg.setAttribute('width', this.sinSVGWidth + "px");
-        svg.setAttribute('height', this.sinSVGHeight + "px");
+        svg.setAttribute('width', this.sinSVGWidth + this.strokeWidth  + "px");
+        svg.setAttribute('height', this.sinSVGHeight + this.strokeWidth  + "px");
 		path.setAttribute('d', this.describeSinPath());
-		path.style.stroke = "#000000"
-		path.style.strokeWidth = this.strokeWidth
-		sinGroup.appendChild(path)
-		svgHolder.replaceChildren(svg);
-		svg.appendChild(sinGroup)
+		path.style.stroke = "#000000";
+		path.style.strokeWidth = this.strokeWidth;
+		this.sinGroup.appendChild(path);
+		this.svgHolder.replaceChildren(svg);
+		svg.appendChild(this.sinGroup);
+		this.svg = svg;
+		this.svgHolder.style.cursor = "pointer";
+		this.drawHandler()
 
+		this.handle.setAttribute('cx', this.PercentageSinX(0) + this.strokeWidth/2);
+        this.handle.setAttribute('cy', this.PercentageSinY(0));
+
+		this.svgHolder.addEventListener("mousedown", this.clickDown.bind(this), false);
+		this.svgHolder.addEventListener("touchstart", this.clickDown.bind(this), false);
+		this.svgHolder.addEventListener('mousemove', this.clickMove.bind(this), false);
+        this.svgHolder.addEventListener('touchmove', this.clickMove.bind(this), false);
+
+		window.addEventListener("mouseup", this.clickUp.bind(this), false);
+		window.addEventListener('touchend', this.clickUp.bind(this), false);
+	}
+
+	clickDown(event){
+		this.clickHold = true;
+	}
+
+	clickUp(event){
+		this.clickHold = false;
+	}
+
+	boundValue(value, lowerBound, upperBound){
+		if (value > upperBound) value = upperBound;
+		if (value < lowerBound)	value = lowerBound;
+		return value;
+	}
+
+	clickMove(event){
+		if (!this.clickHold) return;
+		let coords = this.getRatioCoords (event)
+		this.percentageValue = this.boundValue(coords[0] * 100, 0 ,100)
+		this.handle.setAttribute('cx', this.PercentageOffsetX(this.percentageValue) + this.strokeWidth/2);
+        this.handle.setAttribute('cy', this.PercentageSinY(this.percentageValue));
+	}
+
+	drawHandler(){
+		this.handle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        this.handle.setAttribute('class', 'sliderHandle');
+        this.handle.setAttribute('cx', this.PercentageOffsetX(this.percentageValue));
+        this.handle.setAttribute('cy', this.PercentageSinY(this.percentageValue));
+        this.handle.setAttribute('r', this.strokeWidth/2);
+        this.handle.style.stroke = "#ff0000";
+        this.handle.style.strokeWidth = 1;
+        this.handle.style.fill = "#ff0000";
+        this.sinGroup.appendChild(this.handle);
+	}
+
+	getRatioCoords(event){
+		let x, y, clientX, clientY;
+        if (window.TouchEvent && e instanceof TouchEvent){
+			// for touch devices
+            clientX = event.touches[0].pageX - window.scrollX;
+            clientY = event.touches[0].pageY - window.scrollY;
+        }else {
+			// for mouse or touchpad devices
+            clientX = event.clientX;
+            clientY = event.clientY;
+        }
+		let BoundingWrapper = this.holderElement.getBoundingClientRect()
+		let boundingSVGHolder = this.svgHolder.getBoundingClientRect();
+		x = clientX - boundingSVGHolder.left;
+        y = clientY - boundingSVGHolder.top;
+
+		return [x/boundingSVGHolder.width, y/boundingSVGHolder.height];
 	}
 
 	generate(){
-		this.generateSVG()
+		this.draw();
 	}
 }
 
